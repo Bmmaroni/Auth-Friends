fe2a3b445808626cddb26915fb1fa74521952a04
Brandon Maroni <brandon-maroni@lambdastudents.com>
2021-01-15 18:50:20 -0700

    created FriendsList component and created axiosWithAuth util

---
+++ friends/src/components/FriendsList.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import axios from 'axios';
+
+class FriendsList extends React.Component() {
+  state = {
+    friendsList: []
+  };
+
+  componentDidMount() {
+    this.getData();
+  }
+
+  getData = () => {
+    axios
+      .get('/data')
+      .then(res => {
+        console.log(res);
+        this.setState({
+          friendsList: res.data
+        })
+      })
+      .catch(err => {console.log(err)});
+  };
+
+  render() {
+    return(
+      <div>
+
+      </div>
+    );
+  }
+};
+
+export default FriendsList;
