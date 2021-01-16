c63c83be5a3b001684d449362a7704ee45d88e70
Brandon Maroni <brandon-maroni@lambdastudents.com>
2021-01-15 18:02:24 -0700

    started to build out App to use react router, created Login and PrivateRoute components

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,10 +1,20 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import { PrivateRoute } from "./components/PrivateRoute";
+
 import './App.css';
 
 function App() {
   return (
-    <div>
-
-    </div>
+    <Router>
+      <div className='App'>
+        <ul>
+          <li>
+            <Link to='/login'>Login</Link>
+          </li>
+        </ul>
+      </div>
+    </Router>
   );
 }
 
