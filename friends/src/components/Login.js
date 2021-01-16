b00f1be054d87b8d12da8a490fa42e76ca317e78
Brandon Maroni <brandon-maroni@lambdastudents.com>
2021-01-15 18:18:16 -0700

    finished building out Login page

--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -9,6 +9,47 @@ class Login extends React.Component {
     }
   };
 
-  
+  handleChanges = e => {
+    e.preventDefault();
+    this.setState({
+      credentials: {
+        ...this.state.credentials,
+        [e.target.name]: e.target.value
+      }
+    });
+  };
+
+  login = e => {
+    e.preventDefault();
+    axios
+      .post('http://localhost:5000/api/login', this.state.credentials)
+      .then(res => {
+        localStorage.setItem('token', res.data);
+        this.props.history.push('/protected');// does this need to be '/FriendsList'? //
+      })
+      .catch(err => console.log(err));
+  };
+
+  render() {
+    return(
+      <div>
+        <form onSubmit={this.login}>
+          <input 
+            type='text'
+            name='username'
+            value={this.state.credentials.username}
+            onChange={this.handleChanges}
+          />
+          <input
+            type='text'
+            name='password'
+            value={this.state.credentials.password}
+            onChange={this.handleChanges}
+          />
+          <button>Login</button>
+        </form>
+      </div>
+    )
+  }
 
 }
